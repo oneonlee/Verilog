fec6b8937ba6128dbcf28ad1631e7445260a9bd0
Lee Dong-geon <peter8739@gmail.com>
2021-12-16 13:50:47 +0900

    2021. 12. 16. final

--- 04. bit_counter/bit_counter_top.v
+++ 04. bit_counter/bit_counter_top.v
@@ -1,19 +1,19 @@
-module bitcount_tb;
+module bitcounter_tb();
+reg [3:0] Data;
+reg clk, rst, LA, LB, EA, EB, w;
+wire a0, z;
+wire [3:0] A;
+wire [2:0] B; 
+bit_counter mybitcount(w, LA, EA, clk, Data, rst, A, B, LB, EB, z, a0);
 
-reg [0:3] D;
-reg EA,Clk,LA,LB;
-reg w;
-wire a;
-wire z;
-wire [0:3] A;
-wire [0:2] B;
-bit_counter cnt(w,LA,EA,Clk,A,LB,B,z,D,a);
-
-initial begin #1 LB=1; #3 LB=0; end
-initial begin w = 0; end
-initial begin D=4'b0110; end
-initial begin Clk=0; repeat(30) #1 Clk=~Clk; end
-initial begin #2 LA=1; #10 LA=0;end
-initial begin #12 EA=1; end
-
+always #3 clk = ~clk; // clk은 3 단위로 바뀌게 설정함
+initial begin
+clk = 0; rst = 1; w = 0; Data = 4'b1011; LA = 4'b0000; EB = 0; EA = 0; // 리셋은 초기 1로 설정해놓고 Data를 7로 설정해놓고 1이 3개인 출력값을 확인하도록 함
+#5 LB = 0; EB = 0; 
+#20 rst = 0; // 리셋이 0이 될 때의 값 변화 확인
+#10 rst = 1; // 다시 리셋을 1로 만들고 정상 진행
+#10 LA = 1; EA = 1; 
+#5 EB = 1; 
+#10 LA = 0;
+end
 endmodule
